264aac956e3ebebdd4ea7dd492c589178db99922
Isaac Arnold <isaacarnold@icloud.com>
2020-12-14 20:08:47 +1300

    Added styling so when a link is clicked in the mobile nav, the overlay disappears

--- js/app.js
+++ js/app.js
@@ -9,14 +9,15 @@ const overlayNav = document.querySelector(".overlay-nav");
 
 // Listens for clicks in the main nav and shows the overlay if the item clicked is the hamburger menu icon
 nav.addEventListener("click", (e) => {
-  if (e.target.classList == "hamburger") {
+  if (e.target === hamburger) {
     overlayNav.style.display = "";
   }
 });
 
 // Listens for clicks in the overlay nav and closes the overlay if the item clicked is the close icon
 overlayNav.addEventListener("click", (e) => {
-  if (e.target.classList == "close") {
+  if (e.target.classList == "close" || e.target.tagName === "A") {
     overlayNav.style.display = "none";
+    console.log("closed");
   }
 });
