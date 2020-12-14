08e8c6173e249d50b8fa0ae22f84f5acd6c5f3ec
Isaac Arnold <isaacarnold@icloud.com>
2020-12-14 19:26:31 +1300

    Added comments for mobile nav JS

--- js/app.js
+++ js/app.js
@@ -7,12 +7,14 @@ const close = document.querySelector(".close");
 const nav = document.querySelector(".nav");
 const overlayNav = document.querySelector(".overlay-nav");
 
+// Listens for clicks in the main nav and shows the overlay if the item clicked is the hamburger menu icon
 nav.addEventListener("click", (e) => {
   if (e.target.classList == "hamburger") {
     overlayNav.style.display = "";
   }
 });
 
+// Listens for clicks in the overlay nav and closes the overlay if the item clicked is the close icon
 overlayNav.addEventListener("click", (e) => {
   if (e.target.classList == "close") {
     overlayNav.style.display = "none";
