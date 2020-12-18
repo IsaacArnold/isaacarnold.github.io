6b865dde7c3c4f9c96c541834a8400a70aa80ff2
Isaac Arnold <isaacarnold@icloud.com>
2020-12-18 14:07:58 +1300

    Added technology div and interactive JS

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const nav = document.querySelector(".nav");
 const overlayNav = document.querySelector(".overlay-nav");
+const techChevron = document.querySelector(".chevron-right");
+const technologies = document.querySelector(".technologies");
 
 /* ====================================
 Event Listeners
@@ -23,5 +25,40 @@ nav.addEventListener("click", (e) => {
 overlayNav.addEventListener("click", (e) => {
   if (e.target.classList == "close" || e.target.tagName === "A") {
     overlayNav.classList.toggle("overlay-visible");
+  }
+});
+
+technologies.addEventListener("click", (e) => {
+  const techListDiv = document.querySelector(".tech-list");
+  let techHTML = `
+        <svg width="40" height="40">
+            <use xlink:href="#html"/>
+        </svg>
+
+        <svg width="40" height="40">
+            <use xlink:href="#css"/>
+        </svg>
+
+        <svg width="40" height="40">
+            <use xlink:href="#sass"/>
+        </svg>                             
+
+        <svg width="40" height="40">
+            <use xlink:href="#js"/>
+        </svg>
+      `;
+  techListDiv.innerHTML = techHTML;
+
+  // If the click is on the chevron then complete the following
+  if (e.target === techChevron) {
+    //   Toggles in the class to rotate the chevron
+    techChevron.classList.toggle("chevron-down");
+  }
+  if (e.target.classList.contains("chevron-down")) {
+    techListDiv.style.display = "";
+    // This next else if ensures that the button clicked is the chevron icon and elimates the div being removed if the user clicks elsewhere on the page.
+  } else if (e.target === techChevron) {
+    //   Removes the tech div is the chevron-down class is not active
+    techListDiv.style.display = "none";
   }
 });
